47cd3dfeb023767199c07b5d2475596e6890e38a
Juan Araujo <juan110588@gmail.com>
2025-10-28 07:46:51 -0300

    feat: ajustes do package.json e env

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const DB_SEQUELIZE_TABLE = process.env.DB_SEQUELIZE_TABLE;
+const DB_SEQUELIZE_DATABASE = process.env.DB_SEQUELIZE_DATABASE;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 
-const sequelize = new Sequelize(DB_SEQUELIZE_TABLE, DB_USER, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_SEQUELIZE_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'mysql',
 });
