0d3acbccdff5285c2d7f55a35bf9409f0b2576c6
Juan Araujo <juan110588@gmail.com>
2025-10-28 09:38:40 -0300

    feat: db connection aiven extra configurations

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,10 +4,15 @@ const DB_SEQUELIZE_DATABASE = process.env.DB_SEQUELIZE_DATABASE;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
 
 const sequelize = new Sequelize(DB_SEQUELIZE_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port: DB_PORT,
   dialect: 'mysql',
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false }
+  }
 });
 
 try {
