3f88cddfdb690540f8580dd91dfb2d3dd0a593c6
Juan Araujo <juan110588@gmail.com>
2025-10-30 09:13:18 -0300

    feat: add helmet middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const helmet = require('helmet');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
 
 // EXPRESS SETUP
 const conn = require('./db/conn');
@@ -29,6 +29,20 @@ app.use(express.json());
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        "default-src": ["'self'"],
+        "img-src": ["'self'", "data:", "https:"],
+        "script-src": ["'self'", "'unsafe-inline'"],
+        "style-src": ["'self'", "'unsafe-inline'"],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+  })
+);
 
 
 // DATABASE CONNECTION AND SERVER START
